86a38fe1214c829dfa29c9e73697227945bd527a
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2025-06-28 21:43:57 -0400

    Remove Test Data Card from Dashboard

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,22 +3,18 @@ import IntakeCard from "@/components/dashboard/IntakeCard";
 import NoShowRiskCard from "@/components/dashboard/NoShowRiskCard";
 import InsuranceEligibilityCard from "@/components/dashboard/InsuranceEligibilityCard";
 import PreauthCard from "@/components/dashboard/PreauthCard";
-import DummyDataCard from "@/components/dashboard/DummyDataCard";
 
 const Index = () => {
   return (
-    <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+    <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-2">
       <div className="xl:col-span-1 space-y-6">
         <AppointmentsCard />
         <PreauthCard />
+        <InsuranceEligibilityCard />
       </div>
       <div className="xl:col-span-1 space-y-6">
         <NoShowRiskCard />
         <IntakeCard />
-      </div>
-      <div className="xl:col-span-1 space-y-6">
-        <InsuranceEligibilityCard />
-        <DummyDataCard />
       </div>
     </div>
   );
