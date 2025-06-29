e2fbba1a5fd7b6df9a64f75b9a12dbc772f8118c
jpark8215 <50881191+jpark8215@users.noreply.github.com>
2025-06-28 21:27:34 -0400

    Add Dummy Data Button to Dashboard

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,24 @@
-
 import AppointmentsCard from "@/components/dashboard/AppointmentsCard";
 import IntakeCard from "@/components/dashboard/IntakeCard";
 import NoShowRiskCard from "@/components/dashboard/NoShowRiskCard";
 import InsuranceEligibilityCard from "@/components/dashboard/InsuranceEligibilityCard";
 import PreauthCard from "@/components/dashboard/PreauthCard";
+import DummyDataCard from "@/components/dashboard/DummyDataCard";
 
 const Index = () => {
   return (
-    <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-2">
+    <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
       <div className="xl:col-span-1 space-y-6">
         <AppointmentsCard />
         <PreauthCard />
-        <InsuranceEligibilityCard />
       </div>
       <div className="xl:col-span-1 space-y-6">
         <NoShowRiskCard />
         <IntakeCard />
+      </div>
+      <div className="xl:col-span-1 space-y-6">
+        <InsuranceEligibilityCard />
+        <DummyDataCard />
       </div>
     </div>
   );
